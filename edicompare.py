f208780a860e7d5ab345898360cf675e7e1bc280
Ivica Matic <ivica.m@sundance.com>
2021-04-28 18:31:35 +0100

    Update edicompare.py to check valid callsigns

--- edicompare.py
+++ edicompare.py
@@ -1,61 +1,63 @@
-#Software za komparaciju edi logova
-#Usporedba da li isti pozivni znak u dva loga ima isti lokator 
-#i provjera da li rb = ima smisla u obziru na vrijeme veze.
+# Software za komparaciju edi logova
+# Usporedba da li isti pozivni znak u dva loga ima isti lokator
+# i provjera da li rb = ima smisla u obziru na vrijeme veze.
 
 import datetime
+
 from colorama import Fore
 from colorama import Style
 from colorama import init
+
 init(convert=True)
 
 
-#funckcije 
+# funckcije
 
-def ucitajEdi(filename,dest,dest2):
-
+def ucitajEdi(filename, dest, dest2):
     try:
 
-        with open(filename) as fp:  
+        with open(filename) as fp:
             line = fp.readline()
             while line:
-                line = fp.readline()  #za svaki red u edi fajlu
-                if line.endswith(';;;;\n'): #ako zavrsava sa ;;;;(novi red)
-                    line = line.split(";") # razdvoji qso redak sa ;
+                line = fp.readline()  # za svaki red u edi fajlu
+                if line.endswith(';;;;\n'):  # ako zavrsava sa ;;;;(novi red)
+                    line = line.split(";")  # razdvoji qso redak sa ;
 
-                    time = "20" + line[0]+line[1] #na datum dodaj 20 ispred
-                    time = datetime.datetime.strptime(time, '%Y%m%d%H%M') #pretvori iz teksta u pravi format datuma
-                    #izvuci sljedece podatke s obzirom na razmake
-                    #qso callsign
+                    time = "20" + line[0] + line[1]  # na datum dodaj 20 ispred
+                    time = datetime.datetime.strptime(time, '%Y%m%d%H%M')  # pretvori iz teksta u pravi format datuma
+                    # izvuci sljedece podatke s obzirom na razmake
+                    # qso callsign
                     callsign = line[2]
-                    #poslani raport
+                    # poslani raport
                     rstsent = line[4]
                     r_sent = line[5]
-                    #primljeni raport
+                    # primljeni raport
                     rstreceived = line[6]
                     r_received = line[7]
                     r_locator = line[9]
-                    #broj bodova
+                    # broj bodova
                     qrb = line[10]
-                    #print(time,"|",callsign,"|",rstsent,"|",r_sent,"|",rstreceived,"|",r_received,"|",r_locator,"|",qrb)
-                    data = qso(callsign,time,rstsent,r_sent,rstreceived,r_received,r_locator,qrb)
+                    # print(time,"|",callsign,"|",rstsent,"|",r_sent,"|",rstreceived,"|",r_received,"|",r_locator,"|",qrb)
+                    data = qso(callsign, time, rstsent, r_sent, rstreceived, r_received, r_locator, qrb)
 
                     # na kraju spremi u odredjeni spremnik
                     dest[callsign] = data
                     dest2[r_locator] = data
 
     except:
-        print("Ne mogu otvoriti falj ",filename_edi_1)
+        print("Ne mogu otvoriti falj ", filename_edi_1)
         quit()
 
 
-#classevi 
-class edi: #ovaj jos nije implementiran
-    def __init__(self,callsign,locator):
+# classevi
+class edi:  # ovaj jos nije implementiran
+    def __init__(self, callsign, locator):
         self.callsign = callsign
         self.locator = locator
 
+
 class qso:
-    def __init__(self,callsign,timedate,rstsent,r_sent,rstreceived,r_received,r_locator,qrb):
+    def __init__(self, callsign, timedate, rstsent, r_sent, rstreceived, r_received, r_locator, qrb):
         self.callsign = str(callsign)
         self.timedate = str(timedate)
         self.rstsent = int(rstsent)
@@ -64,8 +66,9 @@ class qso:
         self.r_received = int(r_received)
         self.r_locator = str(r_locator)
         self.qrb = str(qrb)
+
     def __str__(self):
-        return(self.callsign)
+        return self.callsign
 
 
 filename_edi_1 = input("Unesi filename prvoga edi loga: ")
@@ -73,50 +76,66 @@ filename_edi_2 = input("Unesi filename drugoga edi loga: ")
 
 print("")
 
-log1 = {} #spremiste za prvi log, moguce korisiti SQL u buducnosti
-log1byLoc ={}
-ucitajEdi(filename_edi_1,log1,log1byLoc)
+log1 = {}  # spremiste za prvi log, moguce korisiti SQL u buducnosti
+log1byLoc = {}
+ucitajEdi(filename_edi_1, log1, log1byLoc)
 
-log2 = {} #spremiste za drugi log, moguce korisitit SQL u buducnosti
+log2 = {}  # spremiste za drugi log, moguce korisitit SQL u buducnosti
 log2byLoc = {}
-ucitajEdi(filename_edi_2,log2,log2byLoc)
+ucitajEdi(filename_edi_2, log2, log2byLoc)
 
-
-#napravi provjeru loga2 referencom u log1, provjeri lokatore
+# napravi provjeru loga2 referencom u log1, provjeri lokatore
 brojGreski = 0
 brojUpozorenja = 0
 
 for i in log1:
-    #za svaki ppozivni znak u logu jedan
+    # za svaki ppozivni znak u logu jedan
 
-    try: 
+    try:
         hasErrors = False
-        log1data = log1[i]  
-        log2data = log2[log1data.callsign] # ako taj isti postoji u logu2 spremi ga u varijable,
-        
-        #te usporedi po lokatoru
-        if(log1data.r_locator == log2data.r_locator) == False:
-            print(Fore.RED +"[",log1data.callsign,"]"+ Style.RESET_ALL +"Lokator u prvome logu -", Fore.MAGENTA + log1data.r_locator + Style.RESET_ALL,",rb =",log1data.r_sent,"se ne poklapa sa lokatorom u drugome logu -",Fore.MAGENTA + log2data.r_locator + Style.RESET_ALL,",rb =",log2data.r_sent)
+        log1data = log1[i]
+        log2data = log2[log1data.callsign]  # ako taj isti postoji u logu2 spremi ga u varijable,
+
+        if not any(map(str.isdigit, log1data.callsign[:3])):
+            # Ako prve tri znamenke pozivnoga znaka ne sadrze ni jedan broj
+            int(log1data.callsign[:3])
+            print(Fore.YELLOW + "[", log1data.r_locator, "]" + Style.RESET_ALL + "Pozivni znak u prvome logu -",
+                  Fore.MAGENTA + log1data.callsign + Style.RESET_ALL, ",rb =", log1data.r_sent,
+                  " ima za prve tri znamenke samo slova")
+            brojUpozorenja += 1
+            hasErrors = True
+
+        # te usporedi po lokatoru
+        if not (log1data.r_locator == log2data.r_locator):
+            print(Fore.RED + "[", log1data.callsign, "]" + Style.RESET_ALL + "Lokator u prvome logu -",
+                  Fore.MAGENTA + log1data.r_locator + Style.RESET_ALL, ",rb =", log1data.r_sent,
+                  "se ne poklapa sa lokatorom u drugome logu -", Fore.MAGENTA + log2data.r_locator + Style.RESET_ALL,
+                  ",rb =", log2data.r_sent)
             brojGreski += 1
             hasErrors = True
 
-        #te usporedi po vremenu i broju veze log1 na log2
+        # te usporedi po vremenu i broju veze log1 na log2
         if (log1data.r_received > log2data.r_received) and (log1data.timedate < log2data.timedate) == True:
-            print(Fore.RED +"[",log1data.callsign,"]"+ Style.RESET_ALL +"Primljeni broj u prvome logu",Fore.MAGENTA +log1data.r_received+ Style.RESET_ALL,"je veci od primljenoga broja -",Fore.MAGENTA +log2data.r_received+ Style.RESET_ALL,"u drugome logu iako je veza u logu 1 ranije odrzana")
-            brojGreski += 1
-            hasErrors = True
-        
-        #te usporedi po vremenu i broju veze log2 na log1
-        if (log1data.r_received < log2data.r_received) and (log1data.timedate > log2data.timedate) == True:
-            print(Fore.RED +"[",log1data.callsign,"]"+ Style.RESET_ALL +"Primljeni broj u drugome logu -",Fore.MAGENTA +log2data.r_received+ Style.RESET_ALL,"je veci od primljenoga broja -",Fore.MAGENTA +log1data.r_received+ Style.RESET_ALL,"u prvome logu iako je veza u logu 2 ranije odrzana")
+            print(Fore.RED + "[", log1data.callsign, "]" + Style.RESET_ALL + "Primljeni broj u prvome logu",
+                  Fore.MAGENTA + log1data.r_received + Style.RESET_ALL, "je veci od primljenoga broja -",
+                  Fore.MAGENTA + log2data.r_received + Style.RESET_ALL,
+                  "u drugome logu iako je veza u logu 1 ranije odrzana")
             brojGreski += 1
             hasErrors = True
 
-        if(hasErrors): #odvoji greske za lakse citanje
+        # te usporedi po vremenu i broju veze log2 na log1
+        if (log1data.r_received < log2data.r_received) and (log1data.timedate > log2data.timedate) == True:
+            print(Fore.RED + "[", log1data.callsign, "]" + Style.RESET_ALL + "Primljeni broj u drugome logu -",
+                  Fore.MAGENTA + log2data.r_received + Style.RESET_ALL, "je veci od primljenoga broja -",
+                  Fore.MAGENTA + log1data.r_received + Style.RESET_ALL,
+                  "u prvome logu iako je veza u logu 2 ranije odrzana")
+            brojGreski += 1
+            hasErrors = True
+
+        if hasErrors:  # odvoji greske za lakse citanje
             print(" ")
     except:
         pass
-
 
 for i in log1byLoc:
     hasWarnings = False
@@ -124,22 +143,24 @@ for i in log1byLoc:
         log1data = log1byLoc[i]
         log2data = log2byLoc[log1data.r_locator]
 
-        if (log1data.callsign == log2data.callsign) == False:
-            print(Fore.YELLOW + "[",log1data.r_locator,"]"+ Style.RESET_ALL +"Pozivni znak u prvome logu -" ,Fore.MAGENTA + log1data.callsign + Style.RESET_ALL ,",rb =",log1data.r_sent,"se ne poklapa sa pozivnim znak u drugome logu -",Fore.MAGENTA + log2data.callsign + Style.RESET_ALL ,",rb =",log2data.r_sent, "iako je lokator jednak")
+        if not (log1data.callsign == log2data.callsign):
+            print(Fore.YELLOW + "[", log1data.r_locator, "]" + Style.RESET_ALL + "Pozivni znak u prvome logu -",
+                  Fore.MAGENTA + log1data.callsign + Style.RESET_ALL, ",rb =", log1data.r_sent,
+                  "se ne poklapa sa pozivnim znak u drugome logu -", Fore.MAGENTA + log2data.callsign + Style.RESET_ALL,
+                  ",rb =", log2data.r_sent, "iako je lokator jednak")
             brojUpozorenja += 1
             hasWarnings = True
 
-
-        if(hasWarnings): #odvoji greske za lakse citanje
+        if hasWarnings:  # odvoji greske za lakse citanje
             print(" ")
     except:
         pass
 
 if brojGreski != 0:
-    print(Fore.RED + "Pronadjeno je",brojGreski,"nepoklapanja u logovima" + Style.RESET_ALL)
+    print(Fore.RED + "Pronadjeno je", brojGreski, "nepoklapanja u logovima" + Style.RESET_ALL)
 if brojUpozorenja != 0:
-    print(Fore.YELLOW + "Pronadjeno je",brojUpozorenja,"upozorenja u logovima"+ Style.RESET_ALL)
-else:
+    print(Fore.YELLOW + "Pronadjeno je", brojUpozorenja, "upozorenja u logovima" + Style.RESET_ALL)
+if not brojGreski and not brojUpozorenja:
     print("Nepoklapanja nisu pronadjena")
 
 input("Pritisni bilo koju tipku za izlaz ")
